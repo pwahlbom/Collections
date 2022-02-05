b48eb8d09edfdafbe0ea73aa2b5811fb93fde899
Philip Wahlbom <philip@wahlbom.net>
2022-02-05 17:39:01 -0500

    Update Program.cs

--- src/Collections/Program.cs
+++ src/Collections/Program.cs
@@ -7,8 +7,47 @@ namespace Collections
 {
     public class Program
     {
+        public class Products
+        {
+            public string Name { get; set; }
+        }
         public static void Main(string[] args)
         {
+            var states = new Dictionary<string, string>();
+            states.Add("ca", "California");
+            states.Add("ga", "Georgia");
+            states.Add("tx", "Texas");
+
+            Console.WriteLine(states["ca"]);
+            Console.ReadLine();
+
+            //var products = new List<Products>();
+            //products.Add(new Products { Name = "Phone" });
+            //products.Add(new Products { Name = "Laptop" });
+
+            //var products = new List<Products>();
+            //products.Add(new Products { Name = "Phone" });
+            //products.Add(new Products { Name = "Laptop" });
+            //products.Add(new Products { Name = "TV" });
+            //products.Add(new Products { Name = "CPU" });
+
+            //var Product1 = products[1];
+            //Console.WriteLine(Product1.Name);
+            //Console.ReadLine();
+
+
+            //var movies = new string[3];
+
+            //movies[0] = "Star Wars";
+            //movies[1] = "King Kong";
+            //movies[2] = "Memento";
+            //movies[3] = "300";
+
+            //var movies = new List();
+            //movies.Add("Star Wars");
+            //movies.Add("King Kong");
+            //movies.Add("Memento");
+
         }
     }
 }
